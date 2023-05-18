ea42f0e9ebee036eeb48970ef5392d9afd69c210
michaapyr <michapyr@gmail.com>
2023-05-18 09:05:18 +0200

    securing

--- corvids/settings.py
+++ corvids/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 try:
-    SECRET_KEY = config("SECRET_KEY")
-except:
     SECRET_KEY = os.environ['SECRET_KEY']
+except:
+    SECRET_KEY = config("SECRET_KEY")
+    
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
